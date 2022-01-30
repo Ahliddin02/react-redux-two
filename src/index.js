697350e66cda3ea64ca58279128fc9986a3ef094
Ahliddin Mazbutov <ahliddin.mazbutov@gmail.com>
2022-01-30 14:17:35 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';  
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import  useReducer  from "./features/user";
+import themeReducer from './features/theme';
+
+const store = configureStore({
+  reducer: {
+      user: useReducer,
+      theme: themeReducer,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App /> 
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
